2fca3eedaad9f371a4cfaa6cf4e498b1b7fa0fd0
BuildTools <unconfigured@null.spigotmc.org>
2020-09-02 18:16:22 -0400

    Set up xcode project

---
+++ Adventure 1/Adventure 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adventure 1
+//
+//  Created by Benjamin Blackwell on 9/2/20.
+//  Copyright © 2020 Tom Gillis SVII. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
