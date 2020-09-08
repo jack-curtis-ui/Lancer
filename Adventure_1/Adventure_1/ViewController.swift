13e4a706bfd2278d4f5494e73367fcdaf400d696
BuildTools <unconfigured@null.spigotmc.org>
2020-09-08 14:10:06 -0400

    Error in instantiating the main view controller

---
+++ Adventure_1/Adventure_1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Adventure_1
+//
+//  Created by Benjamin Blackwell on 9/8/20.
+//  Copyright © 2020 Tom Gillis SVII. All rights reserved.
+//
+
+import SwiftUI
+
+struct ViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewController()
+    }
+}
