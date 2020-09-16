2a43456c15cf2d63cee83153fd7d64c8701e3f07
jack-curtis-ui <jackcurtis316@gmail.com>
2020-09-16 14:55:20 -0400

    Added Control menu to java prototype

--- java_ptype/Lancer.java
+++ java_ptype/Lancer.java
@@ -1,8 +1,13 @@
 //Prototype java file for customer meeting
-
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
 public class Lancer{
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException{
         //Intro to Adventure 1        
+        //Enter data using BufferReader 
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));  
+
         System.out.println("Hi I’m Sammy. I’m going to be your guide on our "+
         "imagiplay today!  I will play the part of the star fleet commander. "+  
         "You will be Lancer Seven.  The Lancer squadron is an elite group of star fighters "+
@@ -26,10 +31,29 @@ public class Lancer{
         "the headset is working and you can hear the music playing.  And let the Imagiplay begin!\n");
         pressAnyKeyToContinue();        
         
+        for(int i = 0; i < 15; i++){
+            if(i < 10) System.out.println("//////////////////////////////////////////////////////////////////////////////////////////////////////");
+            else System.out.println();
+        }
+
+        displayMenu();
+
+        //char (char)input = reader.read();
+
         
     }
+
+
+    public static void displayMenu(){
+        System.out.println("                Welcome     ");
+        System.out.println("           Lancer    Seven     ");
+        for(int i = 0; i < 3; i++) System.out.println();
+        System.out.println("               Controls");
+        System.out.println("    'S'           'C'             'T'");
+        System.out.println("Start Game   Continue Game   Read Tutorial");
+    }
     //Function for using Enter to continue
-    private static void pressAnyKeyToContinue(){ 
+    public static void pressAnyKeyToContinue(){ 
         System.out.println("Press Enter key to continue...");
         try
         {
