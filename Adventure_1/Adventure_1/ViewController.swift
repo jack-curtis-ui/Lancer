0a5160c133a51dbef9a317f7a9156bef6ab030da
Jack Curtis <jpc222@lehigh.edu>
2020-10-01 10:53:04 -0400

    Added Labels and buttons to the viewconroller

--- Adventure_1/Adventure_1/ViewController.swift
+++ Adventure_1/Adventure_1/ViewController.swift
@@ -14,6 +14,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //Label for num torpedos on main
+    @IBOutlet weak var TorpedoCount: UILabel!
+    //Label for num grenades on main
+    @IBOutlet weak var GrenadeCount: UILabel!
+    //Label for num Lazers on main
+    @IBOutlet weak var LazerCount: UILabel!
+    //Label for shield percents
+    @IBOutlet weak var ShieldPercent: UILabel!
+    
+    //Button to activate shields
+    @IBAction func blueButtonPressed(_ sender: UIButton){
+        
+    }
+    //Button to open weapons menu
+    @IBAction func redButtonPressed(_ sender: UIButton){
+        
+    }
+    //button to Scram
+    @IBAction func greenButtonPressed(_ sender: UIButton){
+        
+    }
+    //button to check fuel
+    @IBAction func yellowButtonPressed(_ sender: UIButton){
+        
+    }
 }
