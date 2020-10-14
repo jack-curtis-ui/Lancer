a2e8ed03a44593369e5eb35012190fe768b8714d
Jack Curtis <jpc222@lehigh.edu>
2020-10-14 12:37:30 -0400

    Restarted see new directory

---
+++ Restart/jackPlayground/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  jackPlayground
+//
+//  Created by Jack on 10/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var topBar: UIView!
+    
+    @IBOutlet weak var bottomBar: UIView!
+    
+    @IBAction func shieldButton(_ sender: Any) {
+    }
+    @IBAction func weaponButton(_ sender: Any) {
+    }
+    @IBAction func scramButton(_ sender: Any) {
+    }
+    @IBAction func fuelButton(_ sender: Any) {
+    }
+    
+}
+
