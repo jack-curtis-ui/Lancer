fe65148fa7b7fe9443a3ac3eab15b044e53a5631
Jack Curtis <jpc222@lehigh.edu>
2020-10-15 20:03:15 -0400

    Changed project name

--- Restart/Lancer_7/WeaponVC.swift
+++ Restart/Lancer_7/WeaponVC.swift
@@ -3,7 +3,8 @@
 //  jackPlayground
 //
 //  Created by Jack on 10/15/20.
-//
+//see link for using multi containerview
+//https://spin.atomicobject.com/2015/09/02/switch-container-views/
 
 import UIKit
 import Foundation
--- Restart/Lancer_7Tests/Lancer_7Tests.swift
+++ Restart/Lancer_7Tests/Lancer_7Tests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import jackPlayground
+@testable import Lancer_7
 
-class jackPlaygroundTests: XCTestCase {
+class Lancer_7Tests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
